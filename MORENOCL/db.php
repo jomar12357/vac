dd85b3876fc20d3fde4091a3db8d890ef8c854ec
Frank Martin Moreno Alburqueque <admin@frankmorenoalburqueque.com>
2020-06-26 18:32:44 -0500

    cambios
    nuevos cambios en la data

--- MORENOCL/db.php
+++ MORENOCL/db.php
@@ -5,7 +5,7 @@
 	class db
 	{
 		function conect01(){
-			$con1 = mysqli_connect("","","");
+			$con1 = mysqli_connect("localhost","root","");
 			mysqli_select_db($con1,"");
 			return($con1);
 		}
---
+++ script.sql
@@ -0,0 +1,12 @@
+CREATE TABLE cursos(
+	id 
+	nombre
+	descrip
+	created_at
+	id_created
+	updated_at 
+	id_updated 
+	drop_at 
+	id_drop 
+	status 
+);
