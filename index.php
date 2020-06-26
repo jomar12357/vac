b1027e6071678daeb41b8dae9233ca97e484470e
Frank Martin Moreno Alburqueque <admin@frankmorenoalburqueque.com>
2020-06-26 15:49:48 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>VAC</title>
+</head>
+<body>
+	<script src="https://gist.github.com/fmorenoadmin/6243fa160636bccee12853d982db1377.js"></script>
+</body>
+</html>
