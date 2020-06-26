a3950356724fd3d71ecf59b94f4d6285c6788881
Frank Martin Moreno Alburqueque <admin@frankmorenoalburqueque.com>
2020-06-26 15:51:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>VAC</title>
+	<title>Metodología VAC</title>
 </head>
 <body>
 	<script src="https://gist.github.com/fmorenoadmin/6243fa160636bccee12853d982db1377.js"></script>
